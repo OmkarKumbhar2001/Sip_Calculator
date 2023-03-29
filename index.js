10dfc195f33a948bde143431c4bb23418f550d36
Omkar Kumbhar <83999016+OmkarKumbhar2001@users.noreply.github.com>
2023-03-29 15:00:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 function calculateSIP(monthlyInvestment, expectedReturn, timePeriod, increasingAmount) {
-  const monthlyRate = expectedReturn / 12 / 100;
-  let totalAmount = 0;
-  let increasingInvestment = monthlyInvestment;
+    const monthlyRate = expectedReturn / 12 / 100;
+    let totalAmount = 0;
+    let increasingInvestment = monthlyInvestment;
+    let invested_amount = 0;
+    for (let i = 1; i <= timePeriod * 12; i++) {
+        totalAmount += increasingInvestment;
+        const interest = totalAmount * monthlyRate;
+        totalAmount += interest;
+        invested_amount += monthlyInvestment;
 
-  for (let i = 1; i <= timePeriod * 12; i++) {
-    totalAmount += increasingInvestment;
-    const interest = totalAmount * monthlyRate;
-    totalAmount += interest;
-
-    if (i % 12 === 0) {
-      increasingInvestment += increasingAmount;
+        if (i % 12 === 0) {
+            increasingInvestment += increasingAmount;
+            console.log(`total investment Each Year Months ${i}==> ${invested_amount.toFixed(2)}`)
+            invested_amount += increasingAmount;
+        }
     }
-  }
-
-  return totalAmount.toFixed(2);
+    return totalAmount.toFixed(2);
 }
-const monthlyInvestment = 5000;
-const expectedReturn = 12;
+const monthlyInvestment = 4000;
+const expectedReturn = 8;
 const timePeriod = 10;
-const increasingAmount = 000;
+const increasingAmount = 1000;
 
 const totalAmount = calculateSIP(monthlyInvestment, expectedReturn, timePeriod, increasingAmount);
 
